cea44a37212a48cb1da9be5b17241527a88cd8a3
Luan Gerber Siiss <luangerber666@gmail.com>
2025-05-15 20:47:42 -0300

    parte2

--- src/components/estado_lista/index.js
+++ src/components/estado_lista/index.js
@@ -1,29 +1,59 @@
 import React, { useEffect, useState } from "react";
 import './estado-lista-styles.css';
 import api from "../../services/api";
+import logoEstado from '../../assets/estado.png';
+import {FiEdit, FiTrash } from "react-icons/fi"
 
 export default function Estados() {
     
     const [estados, setEstados] = useState([]);
 
     useEffect(() => {
-        api.get('Estado').then(
-            response => {
-                setEstados(response.data);
-            }
-        )
+        if(estados.length <= 0) {
+            api.get('Estado').then(
+                response => {
+                    setEstados(response.data);
+                }
+            )
+        }
     })
     
     return(
-        <div>
+        <div className="estado-container">
+            <header>
+                <img src={logoEstado} alt="Logo Estado" />
+            </header>
             <h1>Lista de Estados</h1>
-            {
-                estados.map(estado => (
-                    <div>
-                        {estado.sigla} . {estado.nome}<br/>
-                    </div>
-                ))
-            }
+            <table className="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Sigla</th>
+                        <th>Nome</th>
+                        <th className="thOpcoes">Opções</th>
+                    </tr>
+                    
+                </thead>
+                <tbody>
+                    {
+                        estados.map(estado => (
+                            <tr>
+                                <td>{estado.sigla}</td>
+                                <td>{estado.nome}</td>
+                                <td className="tdOpcoes">
+                                    <button type="button">
+                                        <FiEdit size="25" color="#17202a" />
+                                    </button>
+                                    <button type="button">
+                                        <FiTrash size="25" color="#FF5555" />
+                                    </button>
+                                </td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            
+            </table>
+            
         </div>
     );
 }
